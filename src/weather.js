bbd874c844680f4ffa6467047221c08c13200608
bodoszidi <szidibee@gmail.com>
2025-05-09 23:37:48 +0100

    adds new function to display current forecast

--- src/weather.js
+++ src/weather.js
@@ -21,6 +21,7 @@ function updateWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(temperature);
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -57,6 +58,56 @@ function handleSearchSubmit(event) {
 
     searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
+// you need to call getForecast() somewhere and pass in the city like you do with searchCity()
+function getForecast(city) {
+    let apiKey = "fbe0f372ad6btocdfb0c2b3e5a4f5432";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+    let forecastHtml = "";
+
+    console.log('res', response);
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml += `<div class="weather-forecast">
+                        <div class="weather-forecast-day">
+                            <div class="weather-forecast-date">${formatDay(
+                                day.time
+                            )}</div>
+                            <div class="weather-forecast-icon">
+                            <img src="${day.condition.icon_url}" />
+                            </div>
+                            <div class="weather-forecast-temps">
+                                <div class="weather-forecast-temp">
+                                <strong>${Math.round(
+                                    day.temperature.maximum
+                                )}º</strong>
+                                </div>
+                                <div class="weather-forecast-temp">
+                                <strong>${Math.round(
+                                    day.temperature.minimum
+                                )}º</strong>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+        }
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("London");
